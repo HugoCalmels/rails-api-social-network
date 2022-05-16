71b39b8459d9a7c5225dad60747b9687b8bae01f
Hugo Calmels <calmels.hugo@yahoo.fr>
2022-05-16 17:08:01 +0200

    half of the CRUD is done for the posts

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::PostsController < ApplicationController
-  
+  before_action :find_post, only: [:show, :update, :destroy]
   def index
     @posts = Post.all
     render json: @posts
@@ -11,12 +11,11 @@ class Api::V1::PostsController < ApplicationController
   end
 
   def create
-
     @post = Post.new(
       title: params[:post][:title],
       content: params[:post][:content],
       user_id: current_user.id,
-      author: current_user.email
+      author: current_user.username
     )
     #@post = Post.new(post_params)
     if @post.save
@@ -26,10 +25,44 @@ class Api::V1::PostsController < ApplicationController
     end
   end
 
+  def destroy
+    puts "#############################"
+    puts "#############################"
+    puts "#############################"
+    puts @post.user_id
+    puts current_user.id
+    puts "#############################"
+    puts "#############################"
+    puts "#############################"
+    if @post && @post.user_id === current_user.id
+      @post.destroy
+      render json: { message: 'Post successfully deleted.'}, status: 200
+    else 
+      render json: { error: 'Unable to delete fact.'}, status: 400
+    end
+
+  end
+
+
+  def update
+    if @post && @post.user_id === current_user.id
+      @post.update(post_params)
+      render json: {message: 'Fact successfully updated.'}, status: 200
+    else 
+      render json: { error: 'Unable to update fact.'}, status: 400
+    end
+  end
+
+
 
    private
 
    def post_params
     params.require(:post).permit(:user_id, :title, :content)
    end
+
+   def find_post
+    @post = Post.find(params[:id])
+   end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_27_151419) do
+ActiveRecord::Schema.define(version: 2022_04_28_101317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2022_04_27_151419) do
     t.bigint "user_id", null: false
     t.string "title"
     t.text "content"
+    t.string "author"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
@@ -42,8 +43,10 @@ ActiveRecord::Schema.define(version: 2022_04_27_151419) do
     t.string "unconfirmed_email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "posts", "users"
