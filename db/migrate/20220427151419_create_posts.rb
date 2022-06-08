bc6eb45ab6775fd6229550576fc8923d02e6afa8
Hugo Calmels <calmels.hugo@yahoo.fr>
2022-06-08 16:19:10 +0200

    images can now be uploaded

--- db/migrate/20220427151419_create_posts.rb
+++ db/migrate/20220427151419_create_posts.rb
@@ -4,6 +4,7 @@ class CreatePosts < ActiveRecord::Migration[6.1]
       t.references :user, null: false, foreign_key: true
       t.string :title
       t.text :content
+      t.text :image_link
       t.string :author
       t.timestamps
     end
