276e58a25e77273eeaa0c6cf1f25e6420e216c82
Hugo Calmels <calmels.hugo@yahoo.fr>
2022-06-16 00:29:00 +0200

    removed mailing creds for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,9 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.default_url_options = { host: 'http://localhost:3001/', :port => 3001 }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
